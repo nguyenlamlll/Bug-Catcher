efa43ba1517f7d7459200ed0b0900b9e4f1fea62
Lam Le <15520405@gm.uit.edu.vn>
2017-12-31 23:33:14 +0700

    Added build creation feature.

--- BugCatcher/BugCatcher.Service/Models/Commands/CreateBuildCommand.cs
+++ BugCatcher/BugCatcher.Service/Models/Commands/CreateBuildCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
@@ -7,6 +8,7 @@ namespace BugCatcher.Service.Models.Commands
 {
     public class CreateBuildCommand
     {
+        [DisplayName("Version")]
         public string Name { get; set; }
 
         public Guid ProductId { get; set; }
---
+++ BugCatcher/BugCatcher.Web/Pages/Dashboard/Build/Create.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BugCatcher.Service.Abstraction;
+using BugCatcher.Service.Models.Commands;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BugCatcher.Web.Pages.Dashboard.Build
+{
+    public class CreateModel : PageModel
+    {
+        [BindProperty]
+        public CreateBuildCommand Input { get; set; }
+
+        private readonly IHttpContextAccessor httpContextAccessor;
+        private readonly IBuildService buildService;
+        public CreateModel(IBuildService buildService, IHttpContextAccessor httpContextAccessor)
+        {
+            this.buildService = buildService;
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            var productId = Guid.Parse(httpContextAccessor.HttpContext.Request.Query["id"].ToString());
+            try
+            {
+                Input.ProductId = productId;
+                buildService.CreateBuild(Input);
+            }
+            catch (DbUpdateException ex)
+            {
+                ModelState.AddModelError(string.Empty, "Unable to save changes. Please try again." +
+                    ex.Message);
+            }
+           
+            return RedirectToPage("/Dashboard/Product/Details", new { id = productId });
+        }
+    }
+}
